5342e02d2d5582783fc4f8aebc32114a43d37eba
aaaaaaaa <rih28184@gmail.com>
2025-05-25 17:36:21 +0900

    始まり

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/send/', // 👈 GitHubのリポジトリ名に合わせて書く！
-  plugins: [react()],
+    base: process.env.GITHUB_PAGES  // この行を追加
+        ? "send"            // この行を追加
+        : "./",                     // この行を追加
+    plugins: [react()],
 })
